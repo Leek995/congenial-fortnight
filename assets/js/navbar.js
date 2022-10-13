4c2c2ea0c109da1e5c358f88cd93591e1f3a81e7
Malik <willis.malik.r@gmail.com>
2022-10-12 20:22:00 -0500

    color scheme

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -5,21 +5,34 @@ hamburger.addEventListener("click", ()=>{
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
+$('#hamburger').on("click", ()=>{
+    $('main').toggle(".hidden");
+})
 
 $('#hamburger').hover(
     function (){
-        $('#hamburger').css("background-color", "red");
+        $('#hamburger').css("background-color", "red").css("border", "solid 1px grey");
     },
     function (){
-        $('#hamburger').css("background-color", "white");
+        $('#hamburger').css("background-color", "lightcoral");
     }
 )
 
 $('.bi-brightness-low').hover(
     function (){
-        $('.bi-brightness-low').css("background-color", "red")
+        $('.bi-brightness-low').css("background-color", "red").css("border", "solid 1px grey");
     },
     function (){
-        $('.bi-brightness-low').css("background-color", "white")
+        $('.bi-brightness-low').css("background-color", "lightcoral")
     }
 )
+$('.bi-brightness-low').on("click", event=>{
+
+
+    // if($('.bi-brightness-low').css("background-color", "blue")){
+    //     $('body').css("background-color", "purple");
+    // }else if($('.bi-brightness-low').css("background-color", "lightcoral")){
+    //     $('body').css("background-color", "lightcoral");
+    // }
+
+})
