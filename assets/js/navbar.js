aab7f3f4b9a530b50dc396efa03e882f90783126
Malik <willis.malik.r@gmail.com>
2022-10-13 20:58:53 -0500

    navbar complete no styles. Header structure configured

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -8,31 +8,7 @@ hamburger.addEventListener("click", ()=>{
 $('#hamburger').on("click", ()=>{
     $('main').toggle(".hidden");
 })
-
-$('#hamburger').hover(
-    function (){
-        $('#hamburger').css("background-color", "red").css("border", "solid 1px grey");
-    },
-    function (){
-        $('#hamburger').css("background-color", "lightcoral");
-    }
-)
-
-$('.bi-brightness-low').hover(
-    function (){
-        $('.bi-brightness-low').css("background-color", "red").css("border", "solid 1px grey");
-    },
-    function (){
-        $('.bi-brightness-low').css("background-color", "lightcoral")
-    }
-)
 $('.bi-brightness-low').on("click", event=>{
 
-
-    // if($('.bi-brightness-low').css("background-color", "blue")){
-    //     $('body').css("background-color", "purple");
-    // }else if($('.bi-brightness-low').css("background-color", "lightcoral")){
-    //     $('body').css("background-color", "lightcoral");
-    // }
-
 })
+
